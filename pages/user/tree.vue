d30596467a8b536befc4479546c95fa9d314d9d2
Xueg <xueg@ymify.com>
2023-12-08 16:21:38 +0800

    树形结构大小调整

--- pages/user/tree.vue
+++ pages/user/tree.vue
@@ -24,7 +24,7 @@
 							orient: 'TB',
 							// 定义图片
 							symbol: 'circle',
-							symbolSize: 90,
+							symbolSize: 75,
 							// symbolOffset: [0,-30],
 							// 展开的节点 -1，null 或者 undefined 表示所有节点都展开
 							initialTreeDepth: -1,
@@ -35,8 +35,8 @@
 								offset: [0, 0],
 								align: 'center',
 								color: '#FFFFFF',
-								lineHeight: 13,
-								fontSize: 10,
+								lineHeight: 12,
+								fontSize: 9,
 								formatter: (params) => {
 									return params.name.replaceAll('|', '\n');
 								}
@@ -82,10 +82,10 @@
 				const canvas = this.$refs.chart.$el
 				if (this.option.series[0].data[0]) {
 					if (this.option.series[0].data[0].deep <= 3) {
-						this.option.series[0].data[0].deep += 1.2
+						this.option.series[0].data[0].deep += 1.5
 					}
-					canvas.style.width = 140 * this.option.series[0].data[0].widthNum + 'px'
-					canvas.style.height = 120 * this.option.series[0].data[0].deep + 'px'
+					canvas.style.width = 110 * this.option.series[0].data[0].widthNum + 'px'
+					canvas.style.height = 90 * this.option.series[0].data[0].deep + 'px'
 				} else {
 					canvas.style.width = '100vw'
 					canvas.style.height = '90vh'
